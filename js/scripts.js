35bdca3a47e67fc5feef22e25cf4b00740d2f0c1
stephaniehenry <46508253+stephaniehenry@users.noreply.github.com>
2020-03-19 19:16:08 -0700

    added/updated js scripts
    I can't seem to get the lightbox to work though!!!

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-console.log('OK')
+// grid toggle
+const trigger = document.querySelector('#trigger');
+const target = document.querySelector('main');
+
+trigger.addEventListener('click', () => {
+  
+//   All the good stuff
+   target.classList.toggle('grid');
+   
+});
+
+//lightbox
+lightGallery(document.getElementById('grid') , {
+  
+  mode : 'lg-lolipop', 
+  speed : 300,
+  download : false,
+   
+  
+});
